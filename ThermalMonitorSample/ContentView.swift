9bdf5149f29b1523f65c969cb7a98ee3041a73ce
Mitsuharu Emoto <mthr1982@gmail.com>
2023-10-16 18:48:36 +0900

    Initial Commit

---
+++ ThermalMonitorSample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ThermalMonitorSample
+//
+//  Created by Mitsuharu Emoto on 2023/10/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ThermalMonitorSample/ThermalMonitorSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThermalMonitorSampleApp.swift
+//  ThermalMonitorSample
+//
+//  Created by Mitsuharu Emoto on 2023/10/16.
+//
+
+import SwiftUI
+
+@main
+struct ThermalMonitorSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
